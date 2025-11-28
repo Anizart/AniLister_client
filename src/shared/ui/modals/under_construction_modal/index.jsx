7cfcb88a471faf1c7e34ad82b9616dd4798def66
Anizart <gitanizart@gmail.com>
2025-11-28 17:51:41 +0500

    adaptive modal window

--- src/shared/ui/modals/under_construction_modal/index.jsx
+++ src/shared/ui/modals/under_construction_modal/index.jsx
@@ -28,7 +28,6 @@ const UnderConstructionModal = ({ mode, isOpen, onClose }) => {
             width="38"
             height="38"
             viewBox="0 0 38 38"
-            fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
